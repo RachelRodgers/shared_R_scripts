fb3601e37bae9c56ec9e37a6ce42335069a95afa
RachelRodgers <rachelrodgers@wustl.edu>
2017-11-07 16:51:25 -0600

    Adjusted formatting style.

--- 16s_HelperFunctions.R
+++ 16s_HelperFunctions.R
@@ -1,48 +1,57 @@
 #16S_HelperFunctions.R
 
-###########################################################
-# Source required functions and load required libraries. #
-###########################################################
+# Source required functions and load required libraries.
 
 source("R_helper_functions.R")
-#load_R_libraries("ArgumentCheck")
 
-#####################
-# Helper Functions #
-#####################
+# Helper functions.
 
 check_phyloseqObject <- function(phyloseqObject, taxRank = "Phylum") {
-
-  # Perform Checks: 
+  # Performs sanity checks on a phyloseq object.
+  #
+  # Args:
+  #   phyloseqObject: An object of class phyloseq.
+  #   taxRank: The taxonomic rank at which to retrieve unique taxa.
+  #
+  # Returns:
+  #   The sample variables, number of taxa, rank names, and unique taxa of the
+  #     phyloseq object.
   
-  # Check that the phyloseq library is loaded into the calling script's environment.  Don't try to proceed otherwise.
+  # Error handling 
+  # Check that the phyloseq library is loaded
   if (!require("phyloseq", character.only = TRUE)) {
     stop("Please make sure the \"phyloseq\" library is loaded.")
   }
-  
-  # Check that a phyloseq object has been passed in.  Don't try to proceed otherwise.
+  # Check that a phyloseq object has been passed in
   if ((class(phyloseqObject)) != "phyloseq") {
-    stop("Please check that you called this function on an object of type \"phyloseq.\"")
+    stop("Please check that you called this function on an object of type 
+         \"phyloseq.\"")
   }
   
-  # Get the rank names in the phyloseq object and use that to check that the taxRank argument specified is ok.
+  # Get the rank names in the phyloseq object. Check that a user-specified rank
+  #   name is available in the phyloseq object.
   rankNames <- rank_names(phyloseqObject)
   if ((taxRank %in% rankNames) == "FALSE") {
-    warning("Warning: the specified taxonomic rank was not one of the following: ", 
+    warning("Warning: the specified taxonomic rank was not one of the 
+            following: ", 
                   paste(as.character(rankNames), collapse = ", "))
     warning(". Setting taxonomic rank to \"Phylum.\"")
     taxRank = "Phylum"
   }
   
-  # Check the phyloseq object.
+  # Perform sanity checks and write out to user
   sampleVars <- sample_variables(phyloseqObject)
   taxaNumber <- ntaxa(phyloseqObject)
   uniqueTaxa <- get_taxa_unique(phyloseqObject, taxRank)
   
-  writeLines(paste("\nSample Variables:\n\n", paste(as.character(sampleVars), collapse = ", ")))
+  writeLines(paste("\nSample Variables:\n\n", paste(as.character(sampleVars), 
+                                                    collapse = ", ")))
   writeLines(paste("\nTaxa Number:", as.character(taxaNumber)))
-  writeLines(paste("\nRank Names:", paste(as.character(rankNames), collapse = ", ")))
-  writeLines(paste("\nChecked for unique taxa at the ", as.character(taxRank), "level."))
-  writeLines(paste("\nUnique Taxa: ", paste(as.character(uniqueTaxa), collapse = ", ")))
+  writeLines(paste("\nRank Names:", paste(as.character(rankNames), 
+                                          collapse = ", ")))
+  writeLines(paste("\nChecked for unique taxa at the ", 
+                   as.character(taxRank), "level."))
+  writeLines(paste("\nUnique Taxa: ", paste(as.character(uniqueTaxa), 
+                                            collapse = ", ")))
   
 }
