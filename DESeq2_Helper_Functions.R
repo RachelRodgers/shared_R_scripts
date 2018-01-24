077548dcb4fc170ec2980dddeb1cdb7cbd99abcc
RachelRodgers <rachelrodgers@wustl.edu>
2018-01-24 13:30:14 -0600

    Added fxns for simple and volcano plot of biomarkers.

--- DESeq2_Helper_Functions.R
+++ DESeq2_Helper_Functions.R
@@ -1,11 +1,16 @@
 # DESeq2_Helper_Functions.R
 
+# Create the significance table used for plotting
 
-GetBiomarkersTable <- function(physeq, 
+GetBiomarkers <- function(physeq, 
                                groupVar,
                                numerator, 
                                denominator,
                                alpha = 0.05) { 
+  
+  # Returns a list of two objects - the DESeq2 results and the Significance Table (sigTable)
+  # Each component can be accessed with dollar sign notation
+  
   # Create formula from string variable
   formula <- as.formula(paste("~", groupVar, sep = " "))
   # Convert physeq object to DESeq Data Set object
@@ -30,7 +35,86 @@ GetBiomarkersTable <- function(physeq,
   # Change significance table Species column to Families, factorize the column and assign levels by decreasing max log2FC
   ddsSignificantResults$Species <- factor(as.character(ddsSignificantResults$Family),
                                           levels = names(maxFC))
-  return(ddsSignificantResults)
+  biomarkerResults <- list("results" = ddsResults, "biomarkerTable" = ddsSignificantResults)
+  #return(ddsSignificantResults)
+  biomarkerResults
 }
 
 
+
+
+
+CreateBiomarkerPlot <- function(biomarkerTable) {
+  biomarkerPlot <- ggplot(data.frame(biomarkerTable),
+                            aes(x = Family,
+                                y = log2FoldChange,
+                                color = Genus)) +
+    geom_point(aes(size = log10(biomarkerTable$baseMean)), 
+               alpha = 0.7) +
+    geom_hline(yintercept = 0, lwd = 1.5) +
+    ggtitle("default plot title") +
+    theme_bw() +
+    theme(plot.title = element_text(hjust = 0.5),
+          axis.text.x = element_text(angle = 315, hjust = 0)) +
+    labs(size = "log10(base mean)")
+  return(biomarkerPlot)
+}
+
+
+
+
+
+
+CreateBiomarkerVolcano <- function(physeq, biomarkerResults) {
+  # Set up
+  taxonomyTable <- data.table(data.frame(as(tax_table(physeq), "matrix")), 
+                              keep.rownames = TRUE)
+  setnames(taxonomyTable, "rn", "OTU")
+  setkeyv(taxonomyTable, "OTU")
+  
+  resultsDataTable <- data.table(as(biomarkerResults$results, "data.frame"),
+                                      keep.rownames = TRUE)
+  setnames(resultsDataTable, "rn", "OTU")
+  setkeyv(resultsDataTable, "OTU")
+  
+  resultsDataTable <- taxonomyTable[resultsDataTable]
+  resultsDataTable <- resultsDataTable %>%
+    filter(., padj != "NA") %>%
+    mutate(., Significant = padj <alpha)
+  
+  # Create volcano plot object
+  volcano <- ggplot(resultsDataTable,
+                    aes(x = log2FoldChange,
+                    y = -log10(padj))) +
+    geom_point(data = subset(resultsDataTable, resultsDataTable$Significant == FALSE), color = "grey") +
+    geom_point(data = subset(resultsDataTable, resultsDataTable$Significant == TRUE), 
+               aes(color = Phylum, size = baseMean)) +
+    geom_vline(xintercept = 0, lty = 2) +
+    geom_hline(yintercept = -log10(alpha)) +
+    ggtitle("default plot title") +
+    theme_bw() +
+    theme(plot.title = element_text(hjust = 0.5),
+          axis.text.x = element_text(angle = -90, hjust = 0, vjust = 0.5),
+          axis.title = element_text(size = 12),
+          axis.text = element_text(size = 12),
+          legend.text = element_text(size = 12)) 
+  return(volcano)
+}
+
+
+
+## TEST ##
+# how to use
+#
+# Physeq object
+#physeqWTDay21
+#
+# Get biomarker results and significance table from DESeq2
+#biomarkers_WTDay21_NoAbx_Abx <- GetBiomarkers(physeqWTDay21, "Treatment", "No_Antibiotics", "Antibiotics")
+# Create simple plot
+#biomarkerPlot_WTDay21_NoAbx_Abx <- CreateBiomarkerPlot(biomarkers_WTDay21_NoAbx_Abx$biomarkerTable)
+#biomarkerPlot_WTDay21_NoAbx_Abx
+# Create volcano plot
+#biomarkerVolcano_WTDay21_NoAbx_Abx <- CreateBiomarkerVolcano(physeqWTDay21,
+#                                                             biomarkers_WTDay21_NoAbx_Abx)
+#biomarkerVolcano_WTDay21_NoAbx_Abx
